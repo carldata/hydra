88e33604115f8485e14ecd016e28506f81e11f6f
Krzysztof Langner <klangner@gmail.com>
2017-08-17 09:12:07 +0200

    Added logger

--- src/main/scala/carldata/hydra/RTCommandProcessor.scala
+++ src/main/scala/carldata/hydra/RTCommandProcessor.scala
@@ -5,6 +5,7 @@ import carldata.hs.RealTime.{AddAction, RealTimeRecord, RemoveAction}
 import carldata.hydra.ComputationDB.Computation
 import carldata.sf.Compiler.compile
 import carldata.sf.{Core, Interpreter}
+import org.slf4j.LoggerFactory
 import spray.json.JsonParser.ParsingException
 import spray.json._
 
@@ -13,12 +14,14 @@ import spray.json._
   */
 class RTCommandProcessor(computationDB: ComputationDB) {
 
+  private val Log = LoggerFactory.getLogger("Hydra")
+
   /**
     * Process data event. Single event can generate 0 or more then 1 computed events.
     * The number of output events depends on how many computations are defined on given channel
     */
   def process(jsonStr: String): Unit = {
-    println(jsonStr)
+    Log.info(jsonStr)
     deserialize(jsonStr) match {
       case Some(RealTimeRecord(AddAction, calculationId, script, trigger, outputChannel)) =>
         compile(script, Seq(Core.header))
