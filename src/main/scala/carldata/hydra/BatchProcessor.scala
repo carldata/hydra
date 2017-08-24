fbeea313a28a7356ca4a5661b0054d29b18d1962
wenta <jakubwenta@gmail.com>
2017-08-24 09:55:15 +0200

    Fixed #5

--- src/main/scala/carldata/hydra/BatchProcessor.scala
+++ src/main/scala/carldata/hydra/BatchProcessor.scala
@@ -5,7 +5,9 @@ import carldata.hs.Batch.BatchRecordJsonProtocol._
 import carldata.hs.Batch._
 import carldata.hs.Data.DataJsonProtocol._
 import carldata.hs.Data.DataRecord
+import carldata.series.TimeSeries
 import carldata.sf.Compiler.compile
+import carldata.sf.Runtime.Value
 import carldata.sf.core.TimeSeriesModule.TimeSeriesValue
 import carldata.sf.{Interpreter, core}
 import org.slf4j.LoggerFactory
@@ -20,34 +22,27 @@ class BatchProcessor {
   private val Log = LoggerFactory.getLogger("Hydra")
 
   def process(jsonStr: String, db: TimeSeriesDB): Seq[String] = {
-    Log.info(jsonStr)
     deserialize(jsonStr) match {
       case Some(BatchRecord(calculationId, script, inputChannelId, outputChannelId, startDate, endDate)) => {
-        //load data from cassandra
-       // val db = new CassandraDB(ContactPoints(Seq(dbName)).keySpace(keyspace))
-        val ts = Await.result(db.getSeries(inputChannelId, startDate, endDate), Duration.Inf)
+        val inputTs = Await.result(db.getSeries(inputChannelId, startDate, endDate), Duration.Inf)
 
-        //TODO: Send event to event bus about starting batch job
-
-        //compile script
-        //Execute script with the loaded time series
         val result = compile(script, Seq(core.MathModule.header, core.DateTimeModule.header, core.TimeSeriesModule.header))
-          .flatMap(exec => Interpreter(exec).run("main", Seq(TimeSeriesValue(ts))))
-
-        //Save returned time series to the output channel
-        val vs = result.right.get.asInstanceOf[TimeSeriesValue].ts.values
-        val ids = result.right.get.asInstanceOf[TimeSeriesValue].ts.index
+          .flatMap(exec => Interpreter(exec).run("main", Seq(TimeSeriesValue(inputTs))))
+        val resultTs = getTimeSeries(result)
+        val vs = resultTs.values
+        val ids = resultTs.index
 
         ids.zip(vs)
           .map(x => DataRecord(outputChannelId, x._1, x._2))
           .map(serialize)
-
-        //TODO: Send event to event bus that batch job has ended.
-
       }
       case _ => Seq()
     }
 
+  }
+
+  def getTimeSeries(s: Either[String, Value]): TimeSeries[Float] = {
+    s.right.get.asInstanceOf[TimeSeriesValue].ts
   }
 
   /** Convert from json with exception handling */
