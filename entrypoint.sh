38d5404a7b2331ed838f77d044bfdaf5db45195f
wenta <jakubwenta@gmail.com>
2017-10-19 14:40:36 +0200

    Update Dockerfile

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-java -jar /root/hydra.jar --kafka=$Kafka_Broker --db=$Cassandra_Address --keyspace=$Cassandra_Keyspace
+java -jar /root/hydra.jar --kafka=$Kafka_Broker --db=$Cassandra_Address --keyspace=$Cassandra_Keyspace --user=$Cassandra_Username --pass=$Cassandra_Password
 
