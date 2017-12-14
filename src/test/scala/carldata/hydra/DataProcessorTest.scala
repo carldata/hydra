94ef11e238dc4f7c50134b48931e05b6e76cf077
Krzysztof Langner <klangner@gmail.com>
2017-12-14 14:35:52 +0100

    Removed unused code

--- src/test/scala/carldata/hydra/DataProcessorTest.scala
+++ src/test/scala/carldata/hydra/DataProcessorTest.scala
@@ -4,7 +4,6 @@ import java.time.LocalDateTime
 
 import carldata.hs.Data.DataJsonProtocol._
 import carldata.hs.Data.DataRecord
-import carldata.hs.RealTime.{AddRealTimeJob, RealTimeJob}
 import carldata.hydra.Main.jobsDB
 import org.apache.kafka.common.serialization.{Serde, Serdes}
 import org.scalatest._
@@ -14,20 +13,10 @@ import spray.json._
 class DataProcessorTest extends FlatSpec with Matchers {
 
   val now: LocalDateTime = LocalDateTime.now
-  val code: String =
-    """
-      |def f(a: Number): Number = a + 1
-      |def main(xs: TimeSeries): TimeSeries = map(xs, f)
-    """.stripMargin
-
-  val computationSet1: Seq[RealTimeJob] = Seq(
-    AddRealTimeJob("calc1", code, Seq("c0"), "c-out", now, now.plusDays(5))
-  )
 
   val inputSet1 = Seq(
     DataRecord("c0", now, 1)
   )
-
 
   val inputSet5 = Seq(
     DataRecord("c0", now.plusMinutes(1), 1),
